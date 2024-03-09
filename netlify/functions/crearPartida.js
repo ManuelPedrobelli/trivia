1b4c2dea3634c94be22b70a2330caadd34b1ff5b
manuel pedrobelli <manupedrob@gmail.com>
2024-03-08 22:48:06 -0300

    asd

--- netlify/functions/crearPartida.js
+++ netlify/functions/crearPartida.js
@@ -3,7 +3,7 @@ const trivia = require('../trivia.js'); // Importa tu lógica de trivia
 
 exports.handler = async (event, context) => {
   // Construye la ruta absoluta a la carpeta 'data'
-  const absoluteDataFolderPath = path.resolve(__dirname, '..', 'data');
+  const absoluteDataFolderPath = path.resolve(__dirname, '..', '..', 'data');
   const pathPreguntas = path.join(absoluteDataFolderPath, 'preguntas.json');
   
   // Tu lógica para crear una partida
